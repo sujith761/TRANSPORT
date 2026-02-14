72bff1dac4a743237ea2f90e5ae98d00e1c96d48
SUJITH CHINNUSAMY S <sujithchinnusamys.24mca@kongu.edu>
2026-02-14 22:04:11 +0530

    Change API_URL to local development endpoint

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env || 'https://kasc-transport.onrender.com';
+const API_URL = process.env || 'http://localhost:5000/api';
 
 const api = axios.create({
   baseURL: API_URL,
