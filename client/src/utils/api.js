50cc845dcce628551c69f694b4d9ee1b4f99c148
SUJITH CHINNUSAMY S <sujithchinnusamys.24mca@kongu.edu>
2026-02-14 21:54:34 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'https://kasc-transport.onrender.com';
+const API_URL = process.env || 'https://kasc-transport.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
